07b66851f98bc938ce3d35a9d813ba61076586fd
alperkilickaya <48261006+alperkilickaya@users.noreply.github.com>
2021-07-13 20:37:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+const form = document.querySelector('form');
+const input = document.querySelector('#txtTaskName');
+const btnDeleteAll = document.querySelector('#btnDeleteAll');
+const taskList = document.querySelector('#task-list'); 
+
+
+//event'ları çağır
+eventListeners();
+
+function eventListeners() {
+    //tüm eventları sayfa yüklendiğinde çalıştırdım.
+    //form submit event'ı
+    form.addEventListener('submit', addNewItem); // form'a submit event ekledim
+    //tek bir görev silme event'ı
+    taskList.addEventListener('click', deleteItem); //ul'ye click event ekledim.
+    //tüm görevleri silme event'ı
+    btnDeleteAll.addEventListener('click', deleteAllItems); // Hepsini Sil'e click event ekledim.
+
+}
+
+function createItem(text){ // items içindeki itemları li'nin içine yerleştir.
+        // li oluştur
+        const li = document.createElement('li');
+        li.className ='taskItem';
+        li.textContent=text;
+
+        //a oluştur
+        const a = document.createElement('a');
+        a.className = '';
+        a.setAttribute('href','#');
+        a.innerHTML='<i class="fas fa-trash"></i>';
+        a.style.float = "right";
+
+        //button oluştur
+        const button = document.createElement('button');
+        button.className = 'complete';
+        button.textContent="Tamamlandı";
+        button.style.float = "right";
+
+      
+
+        button.addEventListener('click', function(){
+            //li.style.cssText = "text-decoration:line-through; background-color:lightgreen";
+            if (li.style.backgroundColor == "wheat") {
+                li.style.cssText= "background-color: lightgreen; text-decoration:line-through";
+            } else {
+                li.style.cssText= "background-color: wheat; text-decoration:none";
+
+            }
+        })
+
+        //a'yı li'ye ekle
+        li.appendChild(a);
+
+        li.appendChild(button);
+
+        //li'yi ul'ye ekle
+        taskList.appendChild(li); 
+}
+
+// yeni görev ekle
+function addNewItem(e){
+
+    if(input.value !== ''){ // input alanı boş değilse devam et.
+
+        createItem(input.value); // input'a giriş yapıldığında li'ye bu değeri bas
+
+        //input'u temizle
+        input.value='';
+    
+        e.preventDefault(); // forma tıklandığında submit olmasını engelledim. Sayfa yenilenmez.
+    }else{
+        alert('Lütfen Görev Giriniz!')
+    }
+}
+ 
+//tek görevi sil. Bubblingden yararlandım
+function deleteItem(e){
+    if(e.target.className === 'fas fa-trash'){
+        if(confirm('Görev Silinsin mi?')){ // tüm class'ı bu isimli ise
+            e.target.parentElement.parentElement.remove(); //i'nin iki parent üstü li elemanı            
+        }
+        
+    }
+    e.preventDefault()
+}
+
+//tüm görevleri sil. Bubblingden yararlandım
+function deleteAllItems(e){
+    if(confirm('Tüm Görevler Silinsin mi?')){
+        while(taskList.firstChild){ // ul'nin ilk child'ı olduğu sürece
+            taskList.removeChild(taskList.firstChild); // ul'nin ilk child'ını kaldır
+            
+        }
+    e.preventDefault(); // a tag'inin refresh olmasını engelledim. 
+    }
+}
+
+
